cce253e1ae3628781734cc6e0e7572054688ce13
Thomas Marchand <me@thomas-marchand.com>
2021-06-11 21:52:45 +0200

    update nginx

--- web.nix
+++ web.nix
@@ -4,7 +4,7 @@
   security.acme.email = "thomas.marchand@tuta.io";
 
   services.nginx = {
-    enable = false;
+    enable = true;
 
     recommendedGzipSettings = true;
     recommendedOptimisation = true;
