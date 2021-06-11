23d24ca4ceae0feb521422e4070e8667d6e4ef0d
Thomas Marchand <me@thomas-marchand.com>
2021-06-11 14:18:51 +0200

    update kernel

--- configuration.nix
+++ configuration.nix
@@ -10,8 +10,7 @@
   ];
 
   boot = {
-    #kernelParams = [ "net.ifnames=0" "biosdevname=0" ];
-    #kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages_latest;
     cleanTmpDir = true;
   };
 
