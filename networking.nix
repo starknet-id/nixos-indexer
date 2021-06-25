0411a44682bce0290302be90f2190515c215a2b6
Thomas Marchand <me@thomas-marchand.com>
2021-06-25 10:24:29 +0200

    revert xrdp

--- networking.nix
+++ networking.nix
@@ -1,24 +1,9 @@
 { lib, ... }: {
-  services = {
-
-    openssh = {
-      enable = true;
-      ports = [ 9473 ];
-      permitRootLogin = "no";
-      passwordAuthentication = true;
-    };
-
-    xserver = {
-      enable = true;
-      displayManager.sddm.enable = true;
-      desktopManager.plasma5.enable = true;
-    };
-
-    xrdp = {
-      enable = true;
-      defaultWindowManager = "startplasma-x11";
-    };
-
+  services.openssh = {
+    enable = true;
+    ports = [ 9473 ];
+    permitRootLogin = "no";
+    passwordAuthentication = true;
   };
 
   networking = {
@@ -26,8 +11,7 @@
     firewall = {
       enable = false;
       allowPing = true;
-      allowedTCPPorts =
-        [ 80 443 3389 6000 25565 ]; # HTTP, SSL, RDP, TOR, MINECRAFT
+      allowedTCPPorts = [ 80 443 6000 25565 ]; # HTTP, SSL, TOR, MINECRAFT
     };
   };
 }
