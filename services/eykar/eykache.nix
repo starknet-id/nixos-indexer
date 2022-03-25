ed6461b42ddda1ccb840d2bb93705106acf3d2f9
Thomas Marchand <thomas.marchand@tuta.io>
2022-03-25 18:04:38 +0100

    update eykache

--- services/eykar/eykache.nix
+++ services/eykar/eykache.nix
@@ -1,31 +1,5 @@
 { pkgs, ... }:
-with pkgs;
-let
-  mach-nix = import (builtins.fetchGit {
-    url = "https://github.com/DavHau/mach-nix";
-    rev = "bdc97ba6b2ecd045a467b008cff4ae337b6a7a6b";
-  }) {
-    pkgs = import <nixpkgs> { };
-    python = "python39";
-    pypiDataRev = "aa41bbda98d5f4333f24fd7090cc8efd3aa5825c";
-    pypiDataSha256 = "1iv08mhqdp5vq5g1fkig5wy3mvcxbd9ka6xsblsbjs4wdcbwflip";
-  };
-  pyEnv = mach-nix.mkPython rec {
-    requirements = ''
-      cython
-      aiohttp
-      aiohttp_cors
-      psycopg2
-      toml
-      tqdm
-      websockets>=9.1,<10
-    ''; # todo: support ipfshttpclient==0.8.0a2
-    packagesExtra = [ ./web3.py ];
-
-    providers = { _default = "nixpkgs,sdist,wheel"; };
-    ignoreCollisions = true;
-  };
-in {
+ {
   systemd.services.eykache = {
     description = "PROGRAMMESWAG";
     after = [ "network.target" ];
@@ -33,7 +7,7 @@ in {
     serviceConfig = {
       Type = "simple";
       User = "thomas";
-      ExecStart = pyEnv + "/bin/python ./run";
+      ExecStart = "./env/bin/python eykache";
       WorkingDirectory = "/home/thomas/services/eykache";
       Restart = "on-failure";
     };
