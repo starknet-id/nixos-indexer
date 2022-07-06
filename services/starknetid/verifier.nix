8e82559bbe7c9422b2bec642225e3f2f88a132b5
Thomas Marchand <thomas.marchand@tuta.io>
2022-07-06 21:24:22 +0200

    fix docker

--- services/starknetid/verifier.nix
+++ services/starknetid/verifier.nix
@@ -1,17 +1,1 @@
-{ pkgs, ... }:
- {
-  systemd.services.starknetid_verifier = {
-    description = "PROGRAMMESWAG";
-    after = [ "network.target" ];
-
-    serviceConfig = {
-      Type = "simple";
-      User = "thomas";
-      ExecStart = "/home/thomas/services/starknetid/env/bin/python verifier";
-      WorkingDirectory = "/home/thomas/services/starknetid";
-      Restart = "on-failure";
-    };
-
-    wantedBy = [ "multi-user.target" ];
-  };
-}
+{ pkgs, ... }: { virtualisation.docker.enable = true; }
