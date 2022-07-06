4652772eadd395e901d106066e2bf17c0aa71d4f
Thomas Marchand <thomas.marchand@tuta.io>
2022-07-06 22:17:37 +0200

    add indexer

--- services/starknetid/indexer.nix
+++ services/starknetid/indexer.nix
@@ -1,6 +1,33 @@
 { pkgs, ... }: {
-  systemd.services = {
-    docker.enable = true;
+  virtualisation.docker.enable = true;
+  environment.systemPackages = with pkgs; [ docker-compose ];
+  systemd.services.starknetid_apibara = {
+    description = "PROGRAMMESWAG";
+    after = [ "network.target" ];
 
+    serviceConfig = {
+      Type = "simple";
+      User = "thomas";
+      ExecStart = "docker-compose up";
+      WorkingDirectory = "/home/thomas/services/starknetid/indexer";
+      Restart = "on-failure";
+    };
+
+    wantedBy = [ "multi-user.target" ];
+  };
+  systemd.services.starknetid_indexer = {
+    description = "PROGRAMMESWAG";
+    after = [ "network.target" ];
+
+    serviceConfig = {
+      Type = "simple";
+      User = "thomas";
+      ExecStart =
+        "/home/thomas/services/starknetid/indexer/env/bin/python indexer";
+      WorkingDirectory = "/home/thomas/services/starknetid/indexer";
+      Restart = "on-failure";
+    };
+
+    wantedBy = [ "multi-user.target" ];
   };
 }
--- services/starknetid/verifier.nix
+++ services/starknetid/verifier.nix
@@ -1,1 +1,17 @@
-{ pkgs, ... }: { virtualisation.docker.enable = true; }
+{ pkgs, ... }: {
+  systemd.services.starknetid_verifier = {
+    description = "PROGRAMMESWAG";
+    after = [ "network.target" ];
+
+    serviceConfig = {
+      Type = "simple";
+      User = "thomas";
+      ExecStart =
+        "/home/thomas/services/starknetid/verifier/env/bin/python verifier";
+      WorkingDirectory = "/home/thomas/services/starknetid/verifier";
+      Restart = "on-failure";
+    };
+
+    wantedBy = [ "multi-user.target" ];
+  };
+}
