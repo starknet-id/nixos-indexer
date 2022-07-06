9689697e666b7759cc018c63e7186f38c9c015f5
Thomas Marchand <thomas.marchand@tuta.io>
2022-07-06 23:22:52 +0200

    remove docker auto

--- services/starknetid/indexer.nix
+++ services/starknetid/indexer.nix
@@ -1,20 +1,6 @@
 { pkgs, ... }: {
   virtualisation.docker.enable = true;
   environment.systemPackages = with pkgs; [ docker-compose ];
-  systemd.services.starknetid_apibara = {
-    description = "PROGRAMMESWAG";
-    after = [ "network.target" ];
-
-    serviceConfig = {
-      Type = "simple";
-      User = "thomas";
-      ExecStart = pkgs.docker-compose + "/bin/docker-compose up";
-      WorkingDirectory = "/home/thomas/services/starknetid/indexer";
-      Restart = "on-failure";
-    };
-
-    wantedBy = [ "multi-user.target" ];
-  };
   systemd.services.starknetid_indexer = {
     description = "PROGRAMMESWAG";
     after = [ "network.target" ];
