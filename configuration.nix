f42e911e360314fc0c1926d08bf5840cf042e044
Thomas Marchand <thomas.marchand@tuta.io>
2022-07-06 21:20:39 +0200

    load indexer

--- configuration.nix
+++ configuration.nix
@@ -9,6 +9,7 @@
     ./services/eykar/database.nix
     ./services/eykar/eykache.nix
     ./services/starknetid/verifier.nix
+    ./services/starknetid/indexer.nix
     ./services/minecraft.nix
   ];
 
