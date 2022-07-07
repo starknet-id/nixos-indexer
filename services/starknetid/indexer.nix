0527060f68618b069b571e77903e525052c12afb
Thomas Marchand <thomas.marchand@tuta.io>
2022-07-07 08:55:04 +0200

    add apibara

--- services/starknetid/indexer.nix
+++ services/starknetid/indexer.nix
@@ -1,15 +1,24 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let
+  apibara_py = pkgs.python39.withPackages (python-packages:
+    with python-packages; [
+      (callPackage ../py/apibara.nix { })
+      black
+      aiohttp
+      aiohttp-cors
+      toml
+    ]);
+in {
   virtualisation.docker.enable = true;
   environment.systemPackages = with pkgs; [ docker-compose ];
-  systemd.services.starknetid_indexer = {
+  systemd.services.dear = {
     description = "PROGRAMMESWAG";
     after = [ "network.target" ];
 
     serviceConfig = {
       Type = "simple";
       User = "thomas";
-      ExecStart =
-        "/home/thomas/services/starknetid/indexer/env/bin/python indexer";
+      ExecStart = apibara_py + "/bin/python indexer";
       WorkingDirectory = "/home/thomas/services/starknetid/indexer";
       Restart = "on-failure";
     };
